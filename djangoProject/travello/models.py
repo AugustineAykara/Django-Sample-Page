eb95ecb358276830c4123ae1dcefbd79b30d7455
augustineaykara <augustineaykara@gmail.com>
2020-03-18 00:15:25 +0530

    upload via git

--- djangoProject/travello/models.py
+++ djangoProject/travello/models.py
@@ -2,10 +2,9 @@ from django.db import models
 
 # Create your models here.
 
-class Destination:
-    id : int
-    name : str
-    img : str
-    descp : str
-    price : int
-    specialOffer : bool
+class Destination(models.Model):
+    name = models.CharField(max_length=50)
+    img = models.ImageField(upload_to='pics')
+    descp = models.TextField()
+    price = models.IntegerField
+    specialOffer = models.BooleanField(default=False)
