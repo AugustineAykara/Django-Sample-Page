2058e06d9c54e6d80d975cfd718ed73db9a82bf7
augustineaykara <augustineaykara@gmail.com>
2020-03-17 12:46:52 +0530

    upload via git

--- djangoProject/travello/views.py
+++ djangoProject/travello/views.py
@@ -9,16 +9,21 @@ def index(request):
     dest1 = Destination()
     dest1.name = "Mumbai"
     dest1.descp = "commercial capital of India"
+    dest1.img = 'destination_1.jpg'
     dest1.price = 700
 
     dest2 = Destination()
     dest2.name = "Kerala"
     dest2.descp = "Gods own country"
+    dest2.img = 'destination_2.jpg'
     dest2.price = 1200
 
     dest3 = Destination()
     dest3.name = "Sikkim"
     dest3.descp = "natural beauty and Buddhism"
+    dest3.img = 'destination_3.jpg'
     dest3.price = 950
 
-    return render(request, 'index.html', {'dest1' : dest1, 'dest2' : dest2, 'dest3' : dest3 })
+    dests = [dest1, dest2, dest3]
+
+    return render(request, 'index.html', {'dests' : dests})
