02be2d3cf8334757e6ec1989996ec98fbe8db38d
augustineaykara <augustineaykara@gmail.com>
2020-03-18 12:03:07 +0530

    upload via git

--- djangoProject/travello/models.py
+++ djangoProject/travello/models.py
@@ -6,5 +6,5 @@ class Destination(models.Model):
     name = models.CharField(max_length=50)
     img = models.ImageField(upload_to='pics')
     descp = models.TextField()
-    price = models.IntegerField
+    price = models.IntegerField()
     specialOffer = models.BooleanField(default=False)
--- djangoProject/travello/views.py
+++ djangoProject/travello/views.py
@@ -6,27 +6,8 @@ from .models import Destination
 
 def index(request):
     
-    dest1 = Destination()
-    dest1.name = "Mumbai"
-    dest1.descp = "commercial capital of India"
-    dest1.img = 'destination_4.jpg'
-    dest1.price = 700
-    dest1.specialOffer = True
+    dests = Destination.objects.all()
 
-    dest2 = Destination()
-    dest2.name = "Kerala"
-    dest2.descp = "Gods own country"
-    dest2.img = 'destination_2.jpg'
-    dest2.price = 1200
-    dest2.specialOffer = False
 
-    dest3 = Destination()
-    dest3.name = "Sikkim"
-    dest3.descp = "natural beauty and Buddhism"
-    dest3.img = 'destination_6.jpg'
-    dest3.price = 950
-    dest2.specialOffer = False
-
-    dests = [dest1, dest2, dest3]
 
     return render(request, 'index.html', {'dests' : dests})
